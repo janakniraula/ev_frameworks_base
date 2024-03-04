rc420head/android_frameworks_base-2

Suppress FingerprintManager logspam
If FingerprintManager#isFingerprintHardwareDetected fails to detect
a fingerprint sensor, it emits a warning in the logcat warn stream.

This failure can only happen in the following cases
 - Device lacks a fingerprint sensor
 - Device has a dying/dead fingerprint sensor
 - The FingerprintManager class lacks the manifest permission to
   connect to the fingerprint hardware via system server

All these are unlikely to be useful scenarios, so disable the logspam

Might make more sense to restrict this function to be called only
a finite times before the system gives up and assumes there is no
usable fingerprint hardware on the device

Change-Id: Ibe32d956ff89379ecacc1348748e3e7272f314d8

core/java/android/hardware/fingerprint/FingerprintManager.java
@@ -769,7 +769,7 @@ public class FingerprintManager {
                 throw e.rethrowFromSystemServer();
             }
         } else {
-            Log.w(TAG, "isFingerprintHardwareDetected(): Service not connected!");
+            if (DEBUG) Log.w(TAG, "isFingerprintHardwareDetected(): Service not connected!");
         }
         return false;
     }

janakniraula/ev_frameworks_base

Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: Jason Edson <jaysonedson@gmail.com>

core/java/android/hardware/fingerprint/FingerprintManager.java
@@ -1199,7 +1199,7 @@ public class FingerprintManager implements BiometricAuthenticator, BiometricFing
                 throw e.rethrowFromSystemServer();
             }
         } else {
-            Slog.w(TAG, "isFingerprintHardwareDetected(): Service not connected!");
+            if (DEBUG) Slog.w(TAG, "isFingerprintHardwareDetected(): Service not connected!");
         }
         return false;
     }